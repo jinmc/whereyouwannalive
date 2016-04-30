8ea34ad3b1384f10923dc047fff774acebc1cbf3
jonc <jcagadas@wharton.upenn.edu>
2016-04-29 20:52:17 -0400

    updated controller and comparison

---
+++ src/whereyoutrynalive/controller/Comparison.java
@@ -0,0 +1,29 @@
+package whereyoutrynalive.controller;
+
+public class Comparison {
+
+	private String name;
+	private Double value;
+	
+	public Comparison(String name, Double value){
+		this.setName(name);
+		this.setValue(value);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+}
--- src/whereyoutrynalive/controller/Controller.java
+++ src/whereyoutrynalive/controller/Controller.java
@@ -15,7 +15,7 @@ public class Controller {
 	public int priorityCount = 0;
 	public int questionCount = 0;
 	public int booleanCount = 0;
-//	ArrayList<CityArea> cities;
+	ArrayList<CityArea> cities;
 	
 
 	
@@ -27,7 +27,7 @@ public class Controller {
 	public Controller(Model model) {
 		File selectedFile = model.start();
 		ArrayList<String> rawData = model.readFile(selectedFile);
-		ArrayList<CityArea> cities = model.makeCities(rawData);
+		this.cities = model.makeCities(rawData);
 		for (CityArea city : cities){
 			System.out.println(city.getCityName());
 		}
@@ -37,23 +37,104 @@ public class Controller {
 		answerArray = new boolean[5];
 		
 		BT = new BinaryTree(cities);
-		
-		
+	
+	}
+	
+	public ArrayList<CityArea> topHalf(ArrayList<CityArea> cities, String questionType, boolean topbot){
+		ArrayList<Comparison> unsorted = new ArrayList<Comparison>();
+		ArrayList<Comparison> sorted = new ArrayList<Comparison>();
+		ArrayList<CityArea> sortedCities = new ArrayList<CityArea>();
+		if (questionType == "Climate"){
+			for (CityArea city : cities){
+				Comparison comparison = new Comparison(city.getCityName(), city.getTemperature());
+				unsorted.add(comparison);
+				System.out.println(comparison.getName());
+			}
+		}
+		if (questionType == "Cost of Living"){
+			for (CityArea city : cities){
+				Comparison comparison = new Comparison(city.getCityName(), city.getCostOfLiving());
+				unsorted.add(comparison);
+			}
+		}
+		if (questionType == "Population Density"){
+			for (CityArea city : cities){
+				Comparison comparison = new Comparison(city.getCityName(), city.getCostOfLiving());
+				unsorted.add(comparison);
+			}
+		}
+		if (questionType == "Job Opportunity"){
+			for (CityArea city : cities){
+				Comparison comparison = new Comparison(city.getCityName(), city.getCostOfLiving());
+				unsorted.add(comparison);
+			}
+		}
+		if (questionType == "Crime Rates"){
+			for (CityArea city : cities){
+				Comparison comparison = new Comparison(city.getCityName(), city.getCostOfLiving());
+				unsorted.add(comparison);
+			}
+		}
+		String topCity = "";
+		Double topValue;
+		int duplicate;
+		for (int i = 0; i < cities.size(); i++){
+			topValue = -10.0;
+			duplicate = 0;
+			for (Comparison city : unsorted){
+				if (city.getValue() >= topValue){
+					duplicate = 0;
+					for (Comparison city2 : sorted){
+						if (city2.getName() == city.getName()){
+							duplicate = 1;
+						}
+					}
+					if (duplicate == 0){
+						topCity = city.getName();
+						topValue = city.getValue();
+					}
+				}
+			}
+			Comparison sortedComparison = new Comparison(topCity, topValue);
+			System.out.println(sortedComparison.getValue());
+			sorted.add(sortedComparison);
+		}
+		CityArea currentCity = null;
+		if (topbot){
+			for (int i = 0; i < cities.size() / 2; i++){
+				String cityName = sorted.get(i).getName();
+				for (CityArea city : cities){
+					if (cityName == city.getCityName()){
+						currentCity = city;
+					}
+				}
+				sortedCities.add(currentCity);
+			}
+		} else {
+			for (int i = cities.size() - 1; i > (cities.size() / 2) - 1; i--){
+				String cityName = sorted.get(i).getName();
+				for (CityArea city : cities){
+					if (cityName == city.getCityName()){
+						currentCity = city;
+					}
+				}
+				sortedCities.add(currentCity);
+			}
+		}
+		for (CityArea city : sortedCities){
+			System.out.println(city.getCityName());
+		}
+		return sortedCities;
 	}
 
 	public static void main(String[] args) {
 		
-		Model model = new Model();
-		
-		 
+		Model model = new Model();	 
 		Controller controller = new Controller(model);
-
 		View view = new View(model, controller);
-
 		view.display1();
-		
-		
-		
+		controller.topHalf(controller.cities, "Climate", false);
+				
 	}
 
 }
